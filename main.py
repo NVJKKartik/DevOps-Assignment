03e3292c65cd18a332c8ccc73d3b094fafbede51
Rohanchidri <160914626+Rohanchidri@users.noreply.github.com>
2024-09-20 00:21:29 +0530

    Added function  draw_x

--- main.py
+++ main.py
@@ -89,11 +89,25 @@ class Tic_Tac_Toe():
 	# author: Aryan T N
 	# Draw O - 
 	# ------------------------------------------------------------------
- def draw_O(self, logical_position):
-        logical_position = np.array(logical_position)
-        # logical_position = grid value on the board
-        # grid_position = actual pixel values of the center of the grid
+    def draw_O(self, logical_position):
+            logical_position = np.array(logical_position)
+            # logical_position = grid value on the board
+            # grid_position = actual pixel values of the center of the grid
+            grid_position = self.convert_logical_to_grid_position(logical_position)
+            self.canvas.create_oval(grid_position[0] - symbol_size, grid_position[1] - symbol_size,
+                                    grid_position[0] + symbol_size, grid_position[1] + symbol_size, width=symbol_thickness,
+                                    outline=symbol_O_color)
+# ------------------------------------------------------------------
+	# author: Rohan Chidri
+	# Draw X - 
+	# ------------------------------------------------------------------
+    def draw_X(self, logical_position):
         grid_position = self.convert_logical_to_grid_position(logical_position)
-        self.canvas.create_oval(grid_position[0] - symbol_size, grid_position[1] - symbol_size,
+        self.canvas.create_line(grid_position[0] - symbol_size, grid_position[1] - symbol_size,
                                 grid_position[0] + symbol_size, grid_position[1] + symbol_size, width=symbol_thickness,
-                                outline=symbol_O_color)
+                                fill=symbol_X_color)
+        self.canvas.create_line(grid_position[0] - symbol_size, grid_position[1] + symbol_size,
+                                grid_position[0] + symbol_size, grid_position[1] - symbol_size, width=symbol_thickness,
+                                fill=symbol_X_color)
+        
+        
