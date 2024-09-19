233fb41ec00ae074598e512b1a065827b41fd712
NVJKKartik <114015823+NVJKKartik@users.noreply.github.com>
2024-09-20 00:27:08 +0530

    fixed the spaces and indentation

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ USE TABS AND NOT SPACES FOR INDENTATION
 
 '''
 
+
 from tkinter import *
 import numpy as np
 
@@ -40,74 +41,74 @@ symbol_O_color = '#0492CF'
 Green_color = '#7BC043'
 
 
-class Tic_Tac_Toe():
-	# ------------------------------------------------------------------
-	# author: PlatJack
-	# Game Initialization Functions - 
-	# ------------------------------------------------------------------
-	def _init_(self):
-		self.window = Tk()
-		self.window.title('Tic-Tac-Toe')
-		self.canvas = Canvas(self.window, width=size_of_board, height=size_of_board)
-		self.canvas.pack()
-		self.window.bind('<Button-1>', self.click)
-		
-		self.initialize_board()
-		self.player_X_turns = True
-		self.board_status = np.zeros(shape=(3, 3))
-		
-		self.player_X_starts = True
-		self.reset_board = False
-		self.gameover = False
-		self.tie = False
-		self.X_wins = False
-		self.O_wins = False
-		
-		self.X_score = 0
-		self.O_score = 0
-		self.tie_score = 0
-	
-	# ------------------------------------------------------------------
-	# author: PlatJack
-	# Window Looping Function - 
-	# ------------------------------------------------------------------
-	def mainloop(self):
-		self.window.mainloop()
-	
-	# ------------------------------------------------------------------
-	# author: PlatJack
-	# Board Initialization Function - 
-	# ------------------------------------------------------------------
-	def initialize_board(self):
-		for i in range(2):
-			self.canvas.create_line((i + 1) * size_of_board / 3, 0, (i + 1) * size_of_board / 3, size_of_board)
-		for i in range(2):
-			self.canvas.create_line(0, (i + 1) * size_of_board / 3, size_of_board, (i + 1) * size_of_board / 3)
+class Tic_Tac_Toe:
+    # ------------------------------------------------------------------
+    # author: PlatJack
+    # Game Initialization Functions -
+    # ------------------------------------------------------------------
+    def __init__(self):
+        self.window = Tk()
+        self.window.title('Tic-Tac-Toe')
+        self.canvas = Canvas(self.window, width=size_of_board, height=size_of_board)
+        self.canvas.pack()
 
+        self.window.bind('<Button-1>', self.click)
 
-# ------------------------------------------------------------------
-	# author: Aryan T N
-	# Draw O - 
-	# ------------------------------------------------------------------
+        self.initialize_board()
+        self.player_X_turns = True
+        self.board_status = np.zeros(shape=(3, 3))
+
+        self.player_X_starts = True
+        self.reset_board = False
+        self.gameover = False
+        self.tie = False
+        self.X_wins = False
+        self.O_wins = False
+
+        self.X_score = 0
+        self.O_score = 0
+        self.tie_score = 0
+
+    # ------------------------------------------------------------------
+    # author: PlatJack
+    # Window Looping Function -
+    # ------------------------------------------------------------------
+    def mainloop(self):
+        self.window.mainloop()
+
+    # ------------------------------------------------------------------
+    # author: PlatJack
+    # Board Initialization Function -
+    # ------------------------------------------------------------------
+    def initialize_board(self):
+        for i in range(2):
+            self.canvas.create_line((i + 1) * size_of_board / 3, 0, (i + 1) * size_of_board / 3, size_of_board)
+        for i in range(2):
+            self.canvas.create_line(0, (i + 1) * size_of_board / 3, size_of_board, (i + 1) * size_of_board / 3)
+
+    # ------------------------------------------------------------------
+    # author: Aryan T N
+    # Draw O -
+    # ------------------------------------------------------------------
     def draw_O(self, logical_position):
-            logical_position = np.array(logical_position)
-            # logical_position = grid value on the board
-            # grid_position = actual pixel values of the center of the grid
-            grid_position = self.convert_logical_to_grid_position(logical_position)
-            self.canvas.create_oval(grid_position[0] - symbol_size, grid_position[1] - symbol_size,
-                                    grid_position[0] + symbol_size, grid_position[1] + symbol_size, width=symbol_thickness,
-                                    outline=symbol_O_color)
-# ------------------------------------------------------------------
-	# author: Rohan Chidri
-	# Draw X - 
-	# ------------------------------------------------------------------
+        logical_position = np.array(logical_position)
+        # logical_position = grid value on the board
+        # grid_position = actual pixel values of the center of the grid
+        grid_position = self.convert_logical_to_grid_position(logical_position)
+        self.canvas.create_oval(grid_position[0] - symbol_size, grid_position[1] - symbol_size,
+                                 grid_position[0] + symbol_size, grid_position[1] + symbol_size,
+                                 width=symbol_thickness, outline=symbol_O_color)
+
+    # ------------------------------------------------------------------
+    # author: Rohan Chidri
+    # Draw X -
+    # ------------------------------------------------------------------
     def draw_X(self, logical_position):
         grid_position = self.convert_logical_to_grid_position(logical_position)
         self.canvas.create_line(grid_position[0] - symbol_size, grid_position[1] - symbol_size,
-                                grid_position[0] + symbol_size, grid_position[1] + symbol_size, width=symbol_thickness,
-                                fill=symbol_X_color)
+                                 grid_position[0] + symbol_size, grid_position[1] + symbol_size,
+                                 width=symbol_thickness, fill=symbol_X_color)
         self.canvas.create_line(grid_position[0] - symbol_size, grid_position[1] + symbol_size,
-                                grid_position[0] + symbol_size, grid_position[1] - symbol_size, width=symbol_thickness,
-                                fill=symbol_X_color)
-        
+                                 grid_position[0] + symbol_size, grid_position[1] - symbol_size,
+                                 width=symbol_thickness, fill=symbol_X_color)
         
