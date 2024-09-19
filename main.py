3bfc7dbe406d23830ba422cb4028e88613260685
NVJKKartik <114015823+NVJKKartik@users.noreply.github.com>
2024-09-20 02:49:38 +0530

    removed TODO

--- main.py
+++ main.py
@@ -1,33 +1,3 @@
-'''
-TODO: Tic Tac Toe Game
--> class Tic_Tac_Toe():
-     def __init__(self): Added by @PlatJack
-     def mainloop(self): Added by @PlatJack
-     def initialize_board(self): Added by @PlatJack
-     def play_again(self):
-     
-     Drawing Functions
-      def draw_O(self, logical_position):
-      def draw_X(self, logical_position):
-      def display_gameover(self):
-      def convert_logical_to_grid_position(self, logical_position):
-      def convert_grid_to_logical_position(self, grid_position):
-      def is_grid_occupied(self, logical_position):
-      def is_winner(self, player):
-      def is_tie(self):
-      def is_gameover(self):
-      def click(self, event):
-      
-Contribution Guidlines:
-Format for adding author name =>
-
-#Author Name
-#What does this function do?
-def function():
-
-USE TABS AND NOT SPACES FOR INDENTATION
-
-'''
 import random
 import math
 from tkinter import *
